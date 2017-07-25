898ec341c6d6568bce61a7ce126d24e2a29e0061
yarlou <YArlou@gomel.iba.by>
2017-07-25 20:49:55 +0300

    sw check

--- sw-register.js
+++ sw-register.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        var promise = navigator.serviceWorker.register(currentLocation + '/sw.js');
-        promise.then(function(registration) {
+        navigator.serviceWorker.register(currentLocation + '/sw.js')
+        .then(function(registration) {
             console.log('[sw] Registered. Scope: ', registration.scope);
         }).catch(function(err) {
             console.log('[sw] Registration failed: ', err);
